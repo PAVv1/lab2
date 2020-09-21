24f5b46ea2b8d2d90e0f6cace4ee8b1c0da21b4d
chakrapanianisetti <70190282+chakrapanianisetti@users.noreply.github.com>
2020-09-21 18:40:22 +0530

    Add files via upload

--- l4_sparsematrix.py
+++ l4_sparsematrix.py
@@ -1,26 +1,17 @@
 #sparsematrix
-#121910313015_chakrapani anisetti
-#defining a function to find transpose
-def transpose(matrix, B):
-    for i in range(N):
-        for j in range(N):
-            B[i][j] = matrix[j][i]
-
-        #initialising normal matrix
+#121910313015_chakrapanianisetti
 matrix=[[2,0,0,0],
-        [0,4,5,0],
+        [3,0,5,0],
         [0,0,7,0],
         [0,4,0,5]]
 for i in matrix:
     for j in i:
         print(j,end=" ")
-
-#take an empty matrix
+    print()
 sparseMatrix=[]
 for i in range(len(matrix)):
     for j in range(len(matrix[1])):
         if matrix[i][j]!=0:
-            #create an temporary matrix to store values
             temp=[]
             temp.append(i)
             temp.append(j)
